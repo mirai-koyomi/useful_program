8d9053d97f5a67fb326342d82d59036290c437ff
藤本　聖也 <seiya_fujimoto@waku-2.com>
2018-05-28 19:03:05 +0900

    feat(millsecConver): 時間の差計算処理の追記

--- write_backup_system/app/models/millsec_convert.rb
+++ write_backup_system/app/models/millsec_convert.rb
@@ -1,6 +1,17 @@
 class MillsecConvert < ApplicationRecord
-  validates :before_time, presence: true
-  validates :after_time, presence: true
-  validates :difference, presence: true
-  validates :millsec, presence: true
+  validates :before_time, :after_time, presence: true
+  validate :get_difference
+
+  private
+  def get_difference
+    if (self.after_time.present? && self.before_time.present?)
+      p self.after_time
+      p self.after_time.to_time.to_i
+      self.difference = self.after_time.to_time.to_i - self.before_time.to_time.to_i
+      self.millsec = self.difference * 1000.0
+    else
+      self.difference = nil
+      self.millsec = nil
+    end
+  end
 end
