9267c4bf57c85e0e4832fce31c1408bef73db40e
藤本　聖也 <seiya_fujimoto@waku-2.com>
2018-06-07 14:04:32 +0900

    edit(dictionary): バリデーション項目の追加と、無題時の仮タイトルへの日付の追加

--- write_backup_system/app/models/dictionary.rb
+++ write_backup_system/app/models/dictionary.rb
@@ -1,12 +1,14 @@
 class Dictionary < ApplicationRecord
   before_validation :check_title, :check_status, :check_publish
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :contents, presence: true
   validates :status, inclusion: { in: ['draft', 'released'] }
   validates :publish, inclusion: { in: [true, false] }
 
   private
   def check_title
     unless ( title? )
-      self.title = '無題のドキュメント'
+      self.title = '無題のドキュメント' + Time.now.strftime('%Y%m%d%H%M%S')
     end
   end
 
