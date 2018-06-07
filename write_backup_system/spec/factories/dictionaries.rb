be72763ab542c8e74d35eaa41336845d5d058581
藤本　聖也 <seiya_fujimoto@waku-2.com>
2018-06-07 12:48:56 +0900

    test(dictionary): テスト実装

--- write_backup_system/spec/factories/dictionaries.rb
+++ write_backup_system/spec/factories/dictionaries.rb
@@ -1,9 +1,20 @@
 FactoryBot.define do
   factory :dictionary do
-    title "MyString"
-    contents "MyText"
-    status "MyString"
-    publish false
-    at_release "2018-06-01 17:43:36"
+    title { Faker::String.random(100) }
+    contents { Faker::Lorem.paragraph(6) }
+    trait :draft do
+      status 'draft'
+      publish false
+    end
+    trait :released do
+      status 'released'
+      publish true
+      released_at { Faker::Date.between(1.year.ago, Date.today) }
+    end
+    trait :private do
+      status 'released'
+      publish false
+      released_at { Faker::Date.between(1.year.ago, Date.today) }
+    end
   end
 end
--- write_backup_system/spec/models/dictionary_spec.rb
+++ write_backup_system/spec/models/dictionary_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Dictionary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ステータスのバリデーション' do
+    example '下書きの場合、有効な状態であること' do
+      dictionary = build(:dictionary, :draft)
+      expect(dictionary.status).to be_valid
+    end
+    example 'リリーズの場合、有効な状態であること' do
+      dictionary = build(:dictionary, :release)
+      expect(dictionary.status).to be_valid
+    end
+    example '下書き、リリース以外の場合、無効な状態であること' do
+      dictionary = build(:dictionary, :draft, status: 'test')
+      expect(dictionary.status).not_to be_valid
+    end
+  end
+  describe '値の整形' do
+    example 'タイトルが空だった場合、自動で仮のタイトルが入ること' do
+      dictionary = build(:dictionary, :draft, title: nil)
+      expect(dictionary.title).not_to be_empty
+    end
+    example 'ステータスが空だった場合、draftが入ること' do
+      dictionary = build(:dictionary, :draft, status: nil)
+      expect(dictionary.status).to eq 'draft'
+    end
+    example '公開設定が空だった場合、falseが入ること' do
+      dictionary = build(:dictionary, :draft, status: nil)
+      expect(dictionary.publish).to be_falsey
+    end
+    example '下書きの場合、公開設定がfalseになること' do
+      dictionary = build(:dictionary, :draft, publish: true)
+      expect(dictionary.publish).to be_falsey
+    end
+  end
 end
