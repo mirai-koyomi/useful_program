d44076543b03cd5b0fec9437adf8bdd42e7de1df
藤本　聖也 <seiya_fujimoto@waku-2.com>
2018-06-07 13:50:45 +0900

    model(dictionary): dictionaryクラスを実装。

--- write_backup_system/app/models/dictionary.rb
+++ write_backup_system/app/models/dictionary.rb
@@ -1,2 +1,26 @@
 class Dictionary < ApplicationRecord
+  before_validation :check_title, :check_status, :check_publish
+  validates :status, inclusion: { in: ['draft', 'released'] }
+  validates :publish, inclusion: { in: [true, false] }
+
+  private
+  def check_title
+    unless ( title? )
+      self.title = '無題のドキュメント'
+    end
+  end
+
+  def check_status
+    unless ( status? )
+      self.status = 'draft'
+    end
+  end
+
+  def check_publish
+    if ( self.status === 'draft' )
+      self.publish = false
+    elsif ( self.publish.nil? )
+      self.publish = false
+    end
+  end
 end
