4ba7c310b3724eba435ef00ef1b5464174a71d1b
藤本　聖也 <seiya_fujimoto@waku-2.com>
2018-06-11 13:51:28 +0900

    feat(dictionary): タイトルが初期値の場合のリリース制限のテストを記述。

--- write_backup_system/app/models/dictionary.rb
+++ write_backup_system/app/models/dictionary.rb
@@ -1,25 +1,41 @@
 class Dictionary < ApplicationRecord
-  before_validation :check_title, :check_status, :check_publish
-  validates :title, presence: true, length: { maximum: 100 }
+  before_validation :set_default_title, :set_default_status, :set_default_publish
+  validates :title, presence: true, length: { :maximum => 100 }
   validates :contents, presence: true
-  validates :status, inclusion: { in: ['draft', 'released'] }
-  validates :publish, inclusion: { in: [true, false] }
+  validates :status, inclusion: { :in => ['draft', 'released'] }
+  validates :publish, inclusion: { :in => [true, false] }
+  validate :can_not_published_with_title_default, if: :is_released?
+
+  def is_released?
+    return true if self.status === 'released'
+    false
+  end
+  def is_draft?
+    return true if self.status === 'draft'
+    false
+  end
 
   private
-  def check_title
+  def can_not_published_with_title_default
+    if ( self.title.match(/無題のドキュメント\d+/) || self.title.blank? )
+      errors.add(:title, ": 初期値のタイトルでは公開できません")
+    end
+  end
+
+  def set_default_title
     unless ( title? )
       self.title = '無題のドキュメント' + Time.now.strftime('%Y%m%d%H%M%S')
     end
   end
 
-  def check_status
+  def set_default_status
     unless ( status? )
       self.status = 'draft'
     end
   end
 
-  def check_publish
-    unless ( self.status != 'draft' || self.publish.nil? )
+  def set_default_publish
+    if ( self.status == 'draft' || self.present? )
       self.publish = false
     end
   end
