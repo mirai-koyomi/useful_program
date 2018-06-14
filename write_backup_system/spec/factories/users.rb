4866bb76ed162d05b8c0b94697c8f7df7d196a05
藤本　聖也 <seiya_fujimoto@waku-2.com>
2018-06-14 18:00:09 +0900

    テストデータとコードの追加

--- write_backup_system/spec/factories/users.rb
+++ write_backup_system/spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user do
     screen_name "MyString"
     name "MyString"
-    password_digest "MyString"
+    password "my_password"
+    password_confirmation "my_password"
     description "MyText"
     comment "MyText"
   end
--- write_backup_system/spec/models/user_spec.rb
+++ write_backup_system/spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデートテスト' do
+    context '必須要素が全て入力されている場合' do
+      example '有効な状態であること' do
+        user = build(:user)
+        expect(user).to be_valid
+      end
+    end
+
+    context 'スクリーンネームが未入力の場合' do
+      example '無効な状態であること' do
+        user = build(:user, screen_name: nil)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context '名前が未入力の場合' do
+      example '無効な状態であること' do
+        user = build(:user, name: nil)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'パスワードが未入力の場合' do
+      example '無効な状態であること' do
+        user = build(:user, password: nil)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'パスワード確認が未入力の場合' do
+      example '無効な状態であること' do
+        user = build(:user, password_confirmation: nil)
+        expect(user).not_to be_valid
+      end
+    end
+    context 'パスワードが一致しない場合' do
+      example '無効な状態であること' do
+        user = build(:user, password: "your_password", password_confirmation: "other_password")
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
